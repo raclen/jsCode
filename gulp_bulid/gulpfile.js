7a3fee4688bab9f141118253b7665a1b6119cc06
ralcen <2275025@qq.com>
2015-03-30 18:13:27 +0800

    update

--- gulp_bulid/gulpfile.js
+++ gulp_bulid/gulpfile.js
@@ -1,27 +1,31 @@
 /**
  * Created by XIAODI001 on 2015/3/6.
- * gulp dome
+ * gulp
  */
 console.log('begin ...');
-var gulp = require('gulp'),
-    notify = require('gulp-notify');//提示信息
+var gulp = require('gulp');
+
 
 /**
  * 需要使用什么插件当然要先引入它的模块啦
  */
-var imagemin = require('gulp-imagemin');
+var imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    cache = require('gulp-cache'),
+    notify = require('gulp-notify');//提示信息
 /**
  * 一般事件的定义放在事件调用的上面，这里我们也把'任务'写到前面
  * 下面是一个压缩图片的任务（task）
  */
-gulp.task('img',function(){
+gulp.task('img', function () {
     return gulp.src('src/img/event_life/*')
         .pipe(imagemin({
-            progressive: true
-
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
         }))
         .pipe(gulp.dest('./dest/img/event_life'))
-        .pipe(notify({ message: 'img task ok' }));
+        .pipe(notify({message: 'img task ok'}));
 
 });
 /**
@@ -29,7 +33,7 @@ gulp.task('img',function(){
  *我们只需要把要执行执行的函数都放在这个函数（或者事件，就像jquery的ready）里面
  *
  */
-gulp.task('default',function(){
+gulp.task('default', function () {
     console.log('common in default');
     gulp.run('img');
 
