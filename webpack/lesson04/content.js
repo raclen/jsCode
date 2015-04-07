c5f2bbd2ba2e7216c6067c7900ef95826438cd2c
ralcen <2275025@qq.com>
2015-04-07 10:30:49 +0800

    update

---
+++ webpack/lesson04/content.js
@@ -0,0 +1,5 @@
+/**
+ * Created by XIAODI001 on 2015/4/3.
+ * 这是一个新代码
+ */
+module.exports = 'it work content.js';
---
+++ webpack/lesson04/entry.js
@@ -0,0 +1,6 @@
+/**
+ * Created by XIAODI001 on 2015/4/3.
+ * 这是一个新代码
+ */
+require("!style!css!./style.css");
+document.write(require("./content.js"));
